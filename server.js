e994bffc76a60bb5453ada51c62082e02bc3e5db
evgKireev <bob1_k@mail.ru>
2023-01-31 00:59:47 +0300

    deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3500;
+const PORT = process.env.PORT || 3500;
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
@@ -19,6 +19,6 @@ require('./middleware/passport')(passport);
 const routes = require('./settings/routes');
 routes(app);
 
-app.listen(port, () => {
-  console.log(`App listen on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`App listen on port ${PORT}`);
 });
