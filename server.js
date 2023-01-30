adfd0d48af2f371698095fedac8e4d413f9b9011
evgKireev <bob1_k@mail.ru>
2023-01-31 01:21:07 +0300

    deploy

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
 const cors = require('cors');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(passport.initialize());
-app.use(
-  cors({
-    credentials: true,
-    origin: ['http://localhost:3000'],
-  })
-);
-require('./middleware/passport')(passport);
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(passport.initialize());
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: ['http://localhost:3000'],
+//   })
+// );
+// require('./middleware/passport')(passport);
 
 const routes = require('./settings/routes');
 routes(app);
+
+// app.use(async, (ctx) => {
+//   ctx.body = 'Hello';
+// });
 
 app.listen(PORT, () => {
   console.log(`App listen on port ${PORT}`);
